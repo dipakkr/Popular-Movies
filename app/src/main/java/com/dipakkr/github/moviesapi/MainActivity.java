b6da3ddf1d4f1dfd6927e49bf07678f96b749237
Deepak <raj.deeepak@gmail.com>
2017-06-08 20:06:35 +0530

    Added item on click from fragment

--- app/src/main/java/com/dipakkr/github/moviesapi/MainActivity.java
+++ app/src/main/java/com/dipakkr/github/moviesapi/MainActivity.java
@@ -40,13 +40,9 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static String TAG = MainActivity.class.getSimpleName();
-    private static final String API_KEY = "53873c6fc26c2abac786d7822d2e1a93";
+
     private static int count = 2;
-    private  Context context;
 
-    List<Movie> movies;
-
-    ApIinterface apiService = Apiclient.getClient().create(ApIinterface.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,10 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        if(API_KEY == null){
-            Toast.makeText(this, "Internal Error occured", Toast.LENGTH_SHORT).show();
-            return;
-        }
+
 
         DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
--- app/src/main/java/com/dipakkr/github/moviesapi/fragment/HighRatedMovies.java
+++ app/src/main/java/com/dipakkr/github/moviesapi/fragment/HighRatedMovies.java
@@ -1,23 +1,113 @@
 package com.dipakkr.github.moviesapi.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.dipakkr.github.moviesapi.R;
+import com.dipakkr.github.moviesapi.activity.MovieDetailActivity;
+import com.dipakkr.github.moviesapi.adapter.MovieAdapter;
+import com.dipakkr.github.moviesapi.model.Movie;
+import com.dipakkr.github.moviesapi.model.MovieResponse;
+import com.dipakkr.github.moviesapi.rest.ApIinterface;
+import com.dipakkr.github.moviesapi.rest.Apiclient;
+import com.dipakkr.github.moviesapi.utils.RecyclerViewClickListener;
+
+import java.util.List;
+
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by root on 6/8/17.
  */
 
 public class HighRatedMovies extends Fragment {
+
+    private static final String API_KEY = "53873c6fc26c2abac786d7822d2e1a93";
+
+    private static String TAG = HighRatedMovies.class.getSimpleName();
+
+    private Context context;
+
+    List<Movie> movies;
+
+    ApIinterface apiService = Apiclient.getClient().create(ApIinterface.class);
+
+    RecyclerView recyclerView;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag_highrated,container,false);
+
+
+        if(API_KEY == null){
+            Toast.makeText(context, "Go get a Api key first", Toast.LENGTH_SHORT).show();
+        }
+
+         recyclerView = (RecyclerView)view.findViewById(R.id.recyler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //Using Animator library
+        recyclerView.setItemAnimator(new SlideInUpAnimator());
+
+        //Calling for getting TOP rated movies
+        Call<MovieResponse> responseCall = apiService.getTopRated(API_KEY);
+        responseCall.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                movies = response.body().getMovies();
+
+                //
+                recyclerView.setAdapter(new MovieAdapter(movies,getActivity(),R.layout.list_movie_item));
+                Log.d(TAG,"TOTAL MOVIES  : " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        recyclerView.addOnItemTouchListener(new RecyclerViewClickListener(getContext(), recyclerView, new RecyclerViewClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //Get the id of movies and pass them to detail activity
+
+                String id = movies.get(position).getId();
+                Log.d(TAG,"ID = " + id);
+                Toast.makeText(getActivity(), "Item " + position+ "clicked" , Toast.LENGTH_SHORT).show();
+                Intent detailIntent = new Intent(getActivity(),MovieDetailActivity.class);
+                detailIntent.putExtra(Intent.EXTRA_TEXT,id);
+                detailIntent.putExtra("pos",position);
+                startActivity(detailIntent);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        }));
+
     }
 }
