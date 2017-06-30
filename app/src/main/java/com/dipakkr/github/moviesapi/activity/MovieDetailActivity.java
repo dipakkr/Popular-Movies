1407032f7f77fca5a3e14d7cd7e5795f046fb3cc
Deepak <raj.deeepak@gmail.com>
2017-06-30 05:43:08 +0530

    toolbar title and back button bug fixed

--- app/src/main/java/com/dipakkr/github/moviesapi/activity/MovieDetailActivity.java
+++ app/src/main/java/com/dipakkr/github/moviesapi/activity/MovieDetailActivity.java
@@ -40,15 +40,16 @@ import static android.view.View.GONE;
  * Created by deepak on 5/28/17.
  */
 
-public class MovieDetailActivity extends AppCompatActivity {
+public class MovieDetailActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
 
     private static String TAG = MovieDetailActivity.class.getSimpleName();
     private static final String API_KEY = "53873c6fc26c2abac786d7822d2e1a93";
-    private static String  BASE_URL = "https://image.tmdb.org/t/p/w500";
+    private static String BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 
-    @BindView(R.id.collapse_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
-    @BindView(R.id.app_bar) AppBarLayout appBarLayout;
+    @BindView(R.id.collapse_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.app_bar)
 
     TextView mMovieDesc;
     TextView mReleaseDate;
@@ -59,51 +60,48 @@ public class MovieDetailActivity extends AppCompatActivity {
     TextView mVotes;
     FloatingActionButton fab;
 
-    MovieDescription movieDescription;
     ApIinterface apIinterface = Apiclient.getClient().create(ApIinterface.class);
     String id;
+    String movie_name;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
 
-        final Toolbar toolbar = new Toolbar(this);
-        if(toolbar != null){
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
-        id  = bundle.getString("movie_id");
-        final String movie_name = bundle.getString("movie_name");
+        id = bundle.getString("movie_id");
+        movie_name = bundle.getString("movie_name");
         final int pos = bundle.getInt("pos");
-        Log.d(TAG,"PASSED_ID : " + id);
-        Log.d(TAG,"PASSED POS : " + movie_name);
+        Log.d(TAG, "PASSED_ID : " + id);
+        Log.d(TAG, "PASSED POS : " + movie_name);
 
-        TextView mTitle = (TextView)findViewById(R.id.md_name);
-        mTitle.setText(movie_name);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_detail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle(movie_name);
 
-        mMovieDesc = (TextView)findViewById(R.id.movie_desc);
-        mReleaseDate = (TextView)findViewById(R.id.release_date);
-        mPoster = (ImageView)findViewById(R.id.movie_poster);
-        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
-        mLang = (TextView)findViewById(R.id.movie_lang);
-        mRunTime = (TextView)findViewById(R.id.movie_runtime);
-        mVotes = (TextView)findViewById(R.id.movie_vote);
-        fab = (FloatingActionButton)findViewById(R.id.fab);
+        mMovieDesc = (TextView) findViewById(R.id.movie_desc);
+        mReleaseDate = (TextView) findViewById(R.id.release_date);
+        mPoster = (ImageView) findViewById(R.id.movie_poster);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mLang = (TextView) findViewById(R.id.movie_lang);
+        mRunTime = (TextView) findViewById(R.id.movie_runtime);
+        mVotes = (TextView) findViewById(R.id.movie_vote);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setVisibility(View.VISIBLE);
 
 
         progressBar.setVisibility(View.VISIBLE);
 
-        final Call<MovieDescription> movieDescriptionCall = apIinterface.getMovieDes(id,API_KEY);
+        final Call<MovieDescription> movieDescriptionCall = apIinterface.getMovieDes(id, API_KEY);
         movieDescriptionCall.enqueue(new Callback<MovieDescription>() {
             @Override
             public void onResponse(Call<MovieDescription> call, Response<MovieDescription> response) {
-                Log.v(TAG,apIinterface.getMovieDes(id,API_KEY).request().url().toString());
+                Log.v(TAG, apIinterface.getMovieDes(id, API_KEY).request().url().toString());
                 String desc = response.body().getMovie_overview();
                 String date = response.body().getMovie_release_date();
                 String image_path = response.body().getMovie_poster_path();
@@ -123,7 +121,7 @@ public class MovieDetailActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<MovieDescription> call, Throwable t) {
-
+                Toast.makeText(MovieDetailActivity.this, "Error in Network Connectivity", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -137,50 +135,26 @@ public class MovieDetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == android.R.id.home){
+        if (id == android.R.id.home) {
             finish();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-   /* @Override
+    @Override
     public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-        int scroll = -1 ;
-         boolean isshowing = false;
-*//*
-        if(scroll == -1 ){
+        int scroll = -1;
+        boolean isshowing = false;
+
+        if (scroll == -1) {
             scroll = appBarLayout.getTotalScrollRange();
             fab.setVisibility(View.GONE);
-            Toast.makeText(this, "SCroll at full", Toast.LENGTH_SHORT).show();
-        }if(scroll + verticalOffset == 0 ) {
-            collapsingToolbarLayout.setTitle(getString(R.string.app_name));
-            Toast.makeText(this, "Up scrolled", Toast.LENGTH_SHORT).show();
-        }else if(isshowing){
+        }
+        if (scroll + verticalOffset == 0) {
+            collapsingToolbarLayout.setTitle(movie_name);
+        } else if (isshowing) {
             collapsingToolbarLayout.setTitle(" ");
             isshowing = false;
-        }*//*
-    }*/
-
-    private boolean isnetworkConnected(){
-        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm.getActiveNetworkInfo() != null;
-    }
-
-    public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
-
-        @Override
-        public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
-                                   View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-            super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
-                    dyUnconsumed);
-
-            if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
-                child.hide();
-            } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
-                child.show();
-            }
         }
     }
-
 }
